557dba9c247e22d59bc5f2a788bbc1a32a2b6357
1Consumption <hanseop95@gmail.com>
2020-09-20 16:48:54 +0900

    feat: create class MemoryCache
    - Responsible for memory storage space.
    - Images can be saved and removed.
    - Can distinguish expired images.

---
+++ Sources/ImageCache/MemoryCache.swift
@@ -0,0 +1,81 @@
+//
+//  MemoryCache.swift
+//  OlafImageCacher
+//
+//  Created by 신한섭 on 2020/09/19.
+//
+
+import UIKit
+
+public class MemeoryCache {
+    
+    private let storage: NSCache<NSString, ExpirableImage> = NSCache<NSString, ExpirableImage>()
+    // To prevent access while writing to memory.
+    // e.g. store, remove
+    private let lock: NSLock = NSLock()
+    private let expirationDate: Expiration
+    private var keys: Set<NSString> = Set<NSString>()
+    
+    public init(countLitmit: Int = .max, totalCoastLimit: Int = 0, expirationDate: Expiration = .minutes(5)) {
+        storage.countLimit = countLitmit
+        storage.totalCostLimit = totalCoastLimit
+        self.expirationDate = expirationDate
+    }
+    
+    public func cleanExpiredImage() {
+        lock.lock()
+        defer { lock.unlock() }
+        
+        keys.forEach {
+            if let image = storage.object(forKey: $0) {
+                if image.isExpired() {
+                    keys.remove($0)
+                    storage.removeObject(forKey: $0)
+                }
+            } else {
+                keys.remove($0)
+            }
+        }
+    }
+    
+    public func store(image: UIImage, forKey key: NSString) {
+        lock.lock()
+        defer { lock.unlock() }
+        
+        let expirableImage = ExpirableImage(image: image, expiration: expirationDate)
+        storage.setObject(expirableImage, forKey: key)
+        keys.insert(key)
+    }
+    
+    public func remove(forKey key: NSString) {
+        lock.lock()
+        defer { lock.unlock() }
+        
+        storage.removeObject(forKey: key)
+        keys.remove(key)
+    }
+    
+    public func removeAll() {
+        lock.lock()
+        defer { lock.unlock() }
+        
+        storage.removeAllObjects()
+        keys.removeAll()
+    }
+    
+    public func image(forKey key: NSString) -> ExpirableImage? {
+        guard let expirableImage = storage.object(forKey: key) else { return nil }
+        guard !expirableImage.isExpired() else { return nil }
+        
+        expirableImage.modifyExpirationDate(expiration: expirationDate)
+        
+        return expirableImage
+    }
+    
+    public func isCached(forKey key: NSString) -> Bool {
+        guard let _ = image(forKey: key) else {
+            return false
+        }
+        return true
+    }
+}
