9954209e103cfcce8e0d922280c5569f486bb998
1Consumption <hanseop95@gmail.com>
2020-09-23 01:30:08 +0900

    feat: add feature - store to Disk Cache
    - create cacheURL using cacheURL(key:) method
    - throw error when catch error in store(data:key:) method
    - `Data` type instance passed as a parameter is stored in cacheURL.
    - When all work is completed, the key is stored in memory.

--- Sources/ImageCache/DiskCache.swift
+++ Sources/ImageCache/DiskCache.swift
@@ -10,7 +10,7 @@ import Foundation
 public class DiskCache {
     
     private let fileManager = FileManager.default
-    private var keys: Set<NSString> = Set<NSString>()
+    private var keys: Set<String> = Set<String>()
     private var cacheDirectory: URL?
     
     public init(cacheName: String) throws {
@@ -22,4 +22,39 @@ public class DiskCache {
         try fileManager.createDirectory(at: documentDirectory.appendingPathComponent(directoryName), withIntermediateDirectories: true, attributes: nil)
         cacheDirectory = documentDirectory.appendingPathComponent(directoryName)
     }
+    
+    private func cacheURL(forKey key: String) -> URL? {
+        return cacheDirectory?.appendingPathComponent(key)
+    }
+    
+    public func store(data: Data, forKey key: String, expiration: Expiration = .never) throws {
+        guard let fileURL = cacheURL(forKey: key) else {
+            return
+        }
+        
+        do {
+            try data.write(to: fileURL)
+        } catch {
+            throw OlafImageCacherError.dataWriteError(fileURL.path)
+        }
+        
+        let attributes: [FileAttributeKey : Date] = [
+            .creationDate: Date(),
+            .modificationDate: expiration.expirationDateFromNow
+        ]
+        
+        do {
+            try fileManager.setAttributes(attributes, ofItemAtPath: fileURL.path)
+        } catch {
+            try? fileManager.removeItem(at: fileURL)
+            throw OlafImageCacherError.cachingError(fileURL.path)
+        }
+        
+        keys.insert(key)
+    }
+}
+
+public enum OlafImageCacherError: Error {
+    case dataWriteError(String)
+    case cachingError(String)
 }
